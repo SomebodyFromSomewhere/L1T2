72cdcc7447d082837fea0fb7292b2b3be31f880a
SomebodyFromSomewhere <andreev2168@gmail.com>
2021-12-15 13:35:40 +0300

    x?

--- L1T2/L1T2.cpp
+++ L1T2/L1T2.cpp
@@ -1,38 +1,61 @@
 ﻿#include <iostream>
+#include <string>
 #include <fstream>
 #include <vector>
+#include <map>
+#include <set>
 
 #include "utils/Error.h"
 #include "utils/utils.h"
 
 Error err;
+struct Pos
+{
+	int x;
+	int y;
+};
+struct Me 
+{
+	int x;
+	int y;
+	int mark;
+};
 
-const int upperBorder = 300;
-const int lowwerBorder = 1;
+Pos start;
+Pos end;
+
+std::vector<Pos> queue;
 
 int N;
-int M;
-int stepCunter = 0;
-
+const int lowwerBorder = 2;
+const int upperBorder = 300;
+ 
 void read(std::string fileName, std::vector<std::vector<int>>& array) {
 	std::ifstream file;
 	file.open(fileName);
 	if (!file.is_open()) {
 		err.addCriticalError("CANNOT READ FILE:" + fileName, "READ");
 	}
-
+	int n = 0;
 	file >> N;
-	file >> M;
 
 	if (N < lowwerBorder && N > upperBorder)
 	{
-		err.addCriticalError("CANNOT READ FILE: OUT OF RANGE[1:300]", "READ");
+		err.addCriticalError("CANNOT READ FILE: OUT OF RANGE[2:300]", "READ");
 	}
 	//READ MAP
-	array.resize(N);
-	for (int i = 0; i < N; i++)
+	array.resize(N + 2);
+	for (int i = 0; i <= N + 1; i++)
 	{
-		for (int j = 0; j < N; j++)
+		for (int j = 0; j <= N + 1; j++)
+		{
+			array[i].push_back(1);
+		}
+		//std::cout << i << std::endl;
+	}
+	for (int i = 1; i < N + 1; i++)
+	{
+		for (int j = 1; j < N + 1; j++)
 		{
 			if (file.eof()) {
 				err.addCriticalError("CANNOT READ FILE: UNEXPECTED EOF", "READ");
@@ -42,26 +65,39 @@ void read(std::string fileName, std::vector<std::vector<int>>& array) {
 			file >> temp;
 
 			if (temp == '.') {
-				array[i].push_back(0);
+				array[i][j] = 0;
 			}
-			else {
-				array[i].push_back(1);
+			else if (temp == '#') {
+				array[i][j] = 1;
 			}
-
-		}
-	}
-	//SET RAFT
-	for (int i = 0; i < M; i++)
-	{
-		for (int j = 0; j < M; j++)
-		{
-			if (array[i][j] != 0)
+			else
 			{
-				err.addCriticalError("CANNOT SET RAFT!", "READ");
+				array[i][j] = 0;
+				if (n == 0)
+				{
+					start = { i, j };
+					n++;
+				}
+				if (n == 1)
+				{
+					end = { i, j };
+					n++;
+				}
+				if (n > 2)
+				{
+					err.addCriticalError("GIVEN MORE THAN TWO POINTS", "READ");
+				}
 			}
-			array[i][j] = 2;
 		}
 	}
+}
+
+void addToQueue(int& u, int& v, bool& res) {
+	if (end.x == u && end.y == v)
+	{
+		res = true;
+	}
+	queue.push_back({ u, v });
 
 }
 
@@ -73,7 +109,6 @@ int main()
 	std::vector<std::vector<int>> map;
     read("INPUT.txt", map);
 	print2dArrayVec(map);
-
 
 
 
